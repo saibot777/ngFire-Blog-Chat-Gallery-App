ec0ca9e46fa8ae7078c14d5e81f42d1abd1b016f
saibot777 <stefantrajkovic777@gmail.com>
2018-03-07 16:50:06 +0100

    core logic added

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/switchMap';
+import {IUser} from "./models/user.model";
+import {AngularFireAuth} from "angularfire2/auth";
+import {AngularFirestore} from "angularfire2/firestore";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthService {
+  user: Observable<IUser>;
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
+    this.user = this.afAuth.authState.switchMap(user => {
+      if (user) {
+        return this.afs.doc<IUser>(`users/${user.uid}`).valueChanges();
+      } else {
+        return Observable.of(null);
+      }
+    });
+  }
+
+  emailSignIn(email: string, password: string) {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then(() => console.log("Signed In!"))
+      .catch(error => console.log(error.message));
+  }
+
+  signOut() {
+    return this.afAuth.auth.signOut()
+      .then(() => {
+        this.router.navigate(['/']);
+      });
+  }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MaterialModule} from "../material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: []
 })
